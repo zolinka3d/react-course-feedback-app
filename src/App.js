a8ba19fb1b89d17233142f261c301764e29f53b6
zolinka3d <zofia.tokarczyk01@gmail.com>
2022-12-20 12:28:01 +0100

    zero warning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 //import Card from './components/shared/Card'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
+//import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import AboutPage from './pages/AboutPage'
